7c43d50ed1c452a8319302057c3edfa969b35e2c
Robert B <rb87pl@gmail.com>
2019-03-23 13:36:58 +0100

    Initial commit

---
+++ src/main/java/pl/sdacademy/projectfull/ProjectfullApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.projectfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectfullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sdacademy/projectfull/ProjectfullApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.projectfull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectfullApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
